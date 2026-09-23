92842e0bedaeb0a50ba9149d5d9be2c6c3b0f5d5
cvanwinkle1208-star <cvanwinkle1208@gmail.com>
2026-09-22 22:56:23 -0400

    feat: include lat/lon in geo object

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -91,7 +91,7 @@ exports.handler = async (event) => {
   if (!geo && messages.length === 1 && ip !== 'unknown') {
     const geoData = await httpsGet(`https://ip-api.com/json/${ip}?fields=city,regionName,country,countryCode,lat,lon`);
     if (geoData && geoData.country) {
-      geo = { city: geoData.city, region: geoData.regionName, country: geoData.country, countryCode: geoData.countryCode, ip };
+      geo = { city: geoData.city, region: geoData.regionName, country: geoData.country, countryCode: geoData.countryCode, lat: geoData.lat, lon: geoData.lon, ip };
     }
   }
 
